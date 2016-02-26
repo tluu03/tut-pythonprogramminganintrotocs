e03ba051f494615fdc494e401704dbe60881c60e
tony <tluu03@gmail.com>
2016-02-25 22:26:17 -0500

    chapter 5 ayy

---
+++ Code/chapter05/month2.py
@@ -0,0 +1,11 @@
+#month2.py 
+# a program to print the month abbreviation, given its number.
+def main():
+    
+        #months is a list used as a lookup table
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+        n = eval(input("Enter a month number (1-12): "))
+
+        print("The month abbreviation is", months[n-1] + ".")
+        
