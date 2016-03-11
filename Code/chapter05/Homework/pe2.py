9e51100c63ff1a81816caa7491569b89b318964f
tony <tluu03@gmail.com>
2016-03-10 23:10:24 -0500

    pe2.py

--- Code/chapter05/Homework/pe2.py
+++ Code/chapter05/Homework/pe2.py
@@ -1,1 +1,12 @@
-# pe2.py
+#pe2.py
+def main():
+    grade=input("What is your grade?1-4?")
+    if grade=="1":
+        grade="A"
+    elif grade=="2":
+        grade="B"
+    elif grade=="3":
+        grade="C"
+    elif grade=="4":
+        grade="F"
+    print("Your grade is:",grade)
