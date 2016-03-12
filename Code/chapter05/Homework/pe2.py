0c485569b0cf6a3b84c6784b5a922878e704f7e9
tony <tluu03@gmail.com>
2016-03-12 00:36:13 -0500

    homework

--- Code/chapter05/Homework/pe2.py
+++ Code/chapter05/Homework/pe2.py
@@ -1,12 +1,12 @@
-#pe2.py
+# pe2.py
+
 def main():
-    grade=input("What is your grade?1-4?")
-    if grade=="1":
-        grade="A"
-    elif grade=="2":
-        grade="B"
-    elif grade=="3":
-        grade="C"
-    elif grade=="4":
-        grade="F"
-    print("Your grade is:",grade)
+    grades  = eval(input("Please your grade numbers: "))
+
+    grade1 = str(grade)
+
+    grades = ["A", "B", "C", "F"]
+    grades = grades[grades-1]
+     
+
+    print("The date is", grade1)
